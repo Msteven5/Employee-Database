87f492300194d6df2339b58daffdc1235cda90d0
Msteven <msdavid55@gmail.com>
2024-01-31 22:37:47 -0600

    initial commit/created file structure

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO department (name)
+VALUES  ("produce"),
+        ("cosmetics"),
+        ("bakery"),
+        ("deli"),
+        ("dairy"),
+        ("customer service");
+
+INSERT INTO roles (title, salary)
+VALUES  ("representative", 45000),
+        ("cashier", 40000),
+        ("salesperson", 55000),
+        ("butcher", 65000),
+        ("baker", 45000),
+        ("dairy associate", 40000);
+
+INSERT INTO employee(first_name, last_name)
+VALUES  ("Matt", "Smith"),
+        ("Mark", "Hamill"),
+        ("Mitchell", "Musso"),
+        ("Mariah", "Carrey"),
+        ("Matthew", "Perry"),
+        ("Matthew", "McConaughey"),
+        ("Michael", "Jackson");
